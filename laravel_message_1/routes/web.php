9ec86719e323adbe3d70b382cc7ba673976c16ac
Damiroquai <daiki.ishida12@gmail.com>
2019-03-08 12:52:47 +0900

    新規追加

---
+++ laravel_message_1/routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/', function() {
+    return view('chat');
+});
+// Route::get('chat', 'ChatController@index');
+
+Route::get('ajax/chat', 'Ajax\ChatController@index'); // メッセージ一覧を取得
+Route::post('ajax/chat', 'Ajax\ChatController@create'); // チャット登録
